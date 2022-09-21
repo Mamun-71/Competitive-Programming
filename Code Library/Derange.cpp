003d751c0d062cbd98198ddbc22ae405be8cb87c
Mamun-71 <63588230+Mamun-71@users.noreply.github.com>
2022-09-21 23:46:36 +0600

    Update Derange.cpp

--- Code Library/Derange.cpp
+++ Code Library/Derange.cpp
@@ -8,3 +8,9 @@ ll Derange(ll n)
 
     return derange[n]=((n-1)%mod*((Derange(n-1))%mod+(Derange(n-2)%mod))%mod)%mod;
 }
+
+/* 
+   all necessary  void funtion need call from particular function.
+   all marco need to be define.
+   Though this precode was tested,check all function that give correct answer
+*/
