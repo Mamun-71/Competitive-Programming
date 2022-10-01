e70c738ded4f533a4505750c47602c1ee5e6b1fc
Mamun-71 <63588230+Mamun-71@users.noreply.github.com>
2022-10-02 00:09:32 +0600

    Create nCr_Dp.cpp

---
+++ Code Library/nCr_Dp.cpp
@@ -0,0 +1,23 @@
+ll ncr[1005][1005];
+void pre_calculate()
+{
+
+    for(ll i = 0; i < 1005; i++)
+    {
+        ncr[i][0] = 1; /// nC0=1
+    }
+
+    for(ll i = 1; i < 1005; i++)
+    {
+        for(ll j = 1; j < 1005; j++)
+        {
+            ncr[i][j] = (ncr[i-1][j]+ncr[i-1][j-1])%mod; ///nCr = (n-1)Cr + (n-1)C(r-1)
+
+        }
+    }
+}
+/* 
+   all necessary  void funtion need call from particular function.
+   all marco need to be define.
+   Though this precode was tested,check all function that give correct answer.
+*/
