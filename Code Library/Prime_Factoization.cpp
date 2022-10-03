ede8dfd7fa52abbb70701b89766097d92b974933
Mamun-71 <63588230+Mamun-71@users.noreply.github.com>
2022-10-03 22:17:15 +0600

    Create Prime_Factoization.cpp

---
+++ Code Library/Prime_Factoization.cpp
@@ -0,0 +1,29 @@
+ll factorize(ll n)
+{
+    /// Seive need to be added
+
+    ll root=sqrt(n);
+
+    for(ll i=0; i<prime.size()&&prime[i]<=root; i++)
+    {
+        if(n%prime[i]==0)
+        {
+            while(n%prime[i]==0)
+            {
+                n=n/prime[i];
+                factors.push_back(prime[i]);
+            }
+            root=sqrt(n);
+        }
+    }
+
+    if(n!=1)
+    {
+        factors.push_back(n);
+    }
+}
+/* 
+   all necessary  void funtion need call from particular function.
+   all marco need to be define.
+   Though this precode was tested,check all function that give correct answer.
+*/
