9378fa95ddc737586fec2ebd4b8c191dababaeb0
Mamun-71 <63588230+Mamun-71@users.noreply.github.com>
2022-10-03 21:49:34 +0600

    Create Seive.cpp

---
+++ Code Library/Seive.cpp
@@ -0,0 +1,38 @@
+int arr[Lim+4];
+vector<int>prime;
+void seive()
+{
+    int root=sqrt(Lim);
+
+    for(int i=3; i<=Lim; i+=2)
+    {
+        arr[i]=0;
+    }
+
+    for(int i=3; i<=root; i+=2)
+    {
+        if(arr[i]==0)
+        {
+            for(int j=i*i; j<=Lim; j+=2*i)
+            {
+                arr[j]=1;
+            }
+        }
+    }
+
+    prime.push_back(2);
+
+    for(int i=3; i<=Lim; i+=2)
+    {
+        if(arr[i]==0)
+        {
+            prime.push_back(i);
+        }
+    }
+
+}
+/* 
+   all necessary  void funtion need call from particular function.
+   all marco need to be define.
+   Though this precode was tested,check all function that give correct answer.
+*/
