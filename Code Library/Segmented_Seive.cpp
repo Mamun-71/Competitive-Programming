5e81e519744f8a37da1d1be9fd1931fc7334df30
Mamun-71 <63588230+Mamun-71@users.noreply.github.com>
2022-10-19 14:49:01 +0600

    Create Segmented_Seive.cpp

---
+++ Code Library/Segmented_Seive.cpp
@@ -0,0 +1,41 @@
+void segmented_seive(ll L ,ll R)
+{
+     /// seive() need to be added in main fuction
+
+    ll root=sqrt(R);
+
+    if(L==1) L++;
+    ll len=R-L+1;
+
+    for(ll i=0; i<len; i++)
+    {
+        arr[i]=0;
+    }
+
+    for(ll i=0; i<prime.size()&&prime[i]<=root; i++)
+    {
+        ll p=prime[i];
+        ll j=p*p;
+
+        if(j<L)
+        {
+            /// To inside in in range L R
+            
+            j=(L+p-1)/p;
+            j=j*p;
+        }
+
+        for(; j<=R; j+=p)
+        {
+            arr[j-L]=1;
+        }
+    }
+
+    for(ll i=0; i<len; i++)
+    {
+        if(arr[i]==0)
+        {
+            sprime.push_back(i+L);
+        }
+    }
+}
