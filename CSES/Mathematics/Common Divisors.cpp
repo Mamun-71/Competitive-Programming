52361428bd1b166fc020229fc7b62894d65b552d
Al-Mamun Salauddin <63588230+Mamun-71@users.noreply.github.com>
2024-02-21 14:45:21 +0600

    Create Common Divisors.cpp

---
+++ CSES/Mathematics/Common Divisors.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+ 
+int arr[1000005];
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int n,root;
+ 
+    memset(arr,0,sizeof arr);
+ 
+    cin>>n;
+ 
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        root=sqrt(x);
+ 
+        for(int j=1; j<=root; j++)
+        {
+            if(x%j==0)
+            {
+                int d=x/j;
+ 
+                if(j==d)
+                {
+                    arr[d]++;
+                }
+                else
+                {
+                    arr[j]++;
+                    arr[d]++;
+                }
+            }
+        }
+    }
+ 
+    int ans=1;
+    for(int i=1; i<=1000000; i++)
+    {
+        if(arr[i]>1)
+        {
+            ans=max(ans,i);
+        }
+    }
+ 
+    cout<<ans<<endl;
+ 
+    return 0;
+}
